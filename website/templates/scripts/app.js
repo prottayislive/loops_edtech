ea1e12309b41a649ea0e6b11fee6bc3476a5b7da
shizu_mz <83330839+shizuka0511@users.noreply.github.com>
2021-10-10 11:25:03 +0700

    Add files via upload

---
+++ website/templates/scripts/app.js
@@ -0,0 +1,196 @@
+const menu =document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const navLogo = document.querySelector('#navbar_logo')
+const body = document.querySelector('bdoy')
+
+// dispay mobelie menu
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+    body.classList.toggle('active')
+
+}
+menu.addEventListener('click',mobileMenu)
+
+// read more and close function 
+function Function_1() {
+    var dot = document.getElementById("dots");
+    var showText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dot.style.display === "none") {
+      dot.style.display = "inline";
+      btnText.innerHTML = "show more"; 
+      showText.style.display = "none";
+    } else {
+      dot.style.display = "none";
+      btnText.innerHTML = "close"; 
+      showText.style.display = "inline";
+    }
+  }
+  function Function_2() {
+    var dot_2 = document.getElementById("dots_2");
+    var showText_2 = document.getElementById("more_2");
+    var btnText_2 = document.getElementById("myBtn_2");
+  
+    if (dot_2.style.display === "none") {
+      dot_2.style.display = "inline";
+      btnText_2.innerHTML = "show more"; 
+      showText_2.style.display = "none";
+    } else {
+      dot_2.style.display = "none";
+      btnText_2.innerHTML = "close"; 
+      showText_2.style.display = "inline";
+    }
+  }
+
+  function Function_3() {
+    var dot_3 = document.getElementById("dots_3");
+    var showText_3 = document.getElementById("more_3");
+    var btnText_3 = document.getElementById("myBtn_3");
+  
+    if (dot_3.style.display === "none") {
+      dot_3.style.display = "inline";
+      btnText_3.innerHTML = "show more"; 
+      showText_3.style.display = "none";
+    } else {
+      dot_3.style.display = "none";
+      btnText_3.innerHTML = "close"; 
+      showText_3.style.display = "inline";
+    }
+  }
+
+  function Function_4() {
+    var dot_4 = document.getElementById("dots_4");
+    var showText_4 = document.getElementById("more_4");
+    var btnText_4 = document.getElementById("myBtn_4");
+  
+    if (dot_4.style.display === "none") {
+      dot_4.style.display = "inline";
+      btnText_4.innerHTML = "show more"; 
+      showText_4.style.display = "none";
+    } else {
+      dot_4.style.display = "none";
+      btnText_4.innerHTML = "close"; 
+      showText_4.style.display = "inline";
+    }
+  }
+
+  function Function_5() {
+    var dot_5 = document.getElementById("dots_5");
+    var showText_5 = document.getElementById("more_5");
+    var btnText_5 = document.getElementById("myBtn_5");
+  
+    if (dot_5.style.display === "none") {
+      dot_5.style.display = "inline";
+      btnText_5.innerHTML = "show more"; 
+      showText_5.style.display = "none";
+    } else {
+      dot_5.style.display = "none";
+      btnText_5.innerHTML = "close"; 
+      showText_5.style.display = "inline";
+    }
+  }
+
+  function Function_6() {
+    var dot_6 = document.getElementById("dots_6");
+    var showText_6 = document.getElementById("more_6");
+    var btnText_6 = document.getElementById("myBtn_6");
+  
+    if (dot_6.style.display === "none") {
+      dot_6.style.display = "inline";
+      btnText_6.innerHTML = "show more"; 
+      showText_6.style.display = "none";
+    } else {
+      dot_6.style.display = "none";
+      btnText_6.innerHTML = "close"; 
+      showText_6.style.display = "inline";
+    }
+  }
+
+  function Function_7() {
+    var dot_7 = document.getElementById("dots_7");
+    var showText_7= document.getElementById("more_7");
+    var btnText_7= document.getElementById("myBtn_7");
+  
+    if (dot_7.style.display === "none") {
+      dot_7.style.display = "inline";
+      btnText_7.innerHTML = "show more"; 
+      showText_7.style.display = "none";
+    } else {
+      dot_7.style.display = "none";
+      btnText_7.innerHTML = "close"; 
+      showText_7.style.display = "inline";
+    }
+  }
+
+  // test
+
+  function save_btn1() {
+    var element = document.getElementById('save_btn1');
+    element.classList.toggle("save_color");
+  }
+  function save_btn2() {
+    var element = document.getElementById('save_btn2');
+    element.classList.toggle("save_color");
+  }
+  function save_btn3() {
+    var element = document.getElementById('save_btn3');
+    element.classList.toggle("save_color");
+  }
+  function save_btn4() {
+    var element = document.getElementById('save_btn4');
+    element.classList.toggle("save_color");
+  }
+  function save_btn5() {
+    var element = document.getElementById('save_btn5');
+    element.classList.toggle("save_color");
+  }
+  function save_btn6() {
+    var element = document.getElementById('save_btn6');
+    element.classList.toggle("save_color");
+  }
+  function save_btn7() {
+    var element = document.getElementById('save_btn7');
+    element.classList.toggle("save_color");
+  }
+  function save_btn8() {
+    var element = document.getElementById('save_btn8');
+    element.classList.toggle("save_color");
+  }
+
+  function like_btn1() {
+    var element = document.getElementById('like_btn1');
+    element.classList.toggle("like_color");
+  }
+  
+  function like_btn2() {
+    var element = document.getElementById('like_btn2');
+    element.classList.toggle("like_color");
+  }
+  function like_btn3() {
+    var element = document.getElementById('like_btn3');
+    element.classList.toggle("like_color");
+  }
+  function like_btn4() {
+    var element = document.getElementById('like_btn4');
+    element.classList.toggle("like_color");
+  }
+  function like_btn5() {
+    var element = document.getElementById('like_btn5');
+    element.classList.toggle("like_color");
+  }
+  function like_btn6() {
+    var element = document.getElementById('like_btn6');
+    element.classList.toggle("like_color");
+  }
+
+  function like_btn7() {
+    var element = document.getElementById('like_btn7');
+    element.classList.toggle("like_color");
+  }
+  function like_btn8() {
+    var element = document.getElementById('like_btn8');
+    element.classList.toggle("like_color");
+  }
